c974f30d383aa6b7a21275ae4fe2ced65e475a57
harshita <harshitareddy.pervala@gmail.com>
2019-07-11 21:37:05 -0400

    uploading all the files

---
+++ Java/src/Core.java
@@ -0,0 +1,19 @@
+
+public class Core {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		harshita h1 = new harshita();
+	h1.harshita();
+	
+		
+		
+		
+System.out.println("my name is harshita");
+		
+		
+		
+		
+	}
+
+}
---
+++ Java/src/harshita.java
@@ -0,0 +1,16 @@
+
+public class harshita {
+
+	static int id = 100;
+	public void harshita(){
+		
+	int data = 10;
+	int x = data +100;
+	System.out.println(x);	
+	}
+	
+	}
+	
+	
+	
+
